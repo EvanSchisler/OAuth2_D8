dc1c69967fe6da5fbb04c653ff2ccb1b7cef2bee
EvanSchisler <eschisler@acromediainc.com>
2015-07-27 17:03:53 -0700

    Finished following entity tutorial. Still a little broken.

---
+++ src/Entity/Controller/ClientListBuilder.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: eschisler
+ * Date: 27/07/15
+ * Time: 4:50 PM
+ */
+
+namespace Drupal\content_entity_example\Entity\Controller;
+use
+
+  Drupal\Core\Entity\EntityInterface;
+use Drupal\Core\Entity\EntityListBuilder;
+use Drupal\Core\Url;
+
+class ClientListBuilder extends EntityListBuilder {
+
+  /**
+   * {@inheritdoc}
+   *
+   * We override ::render() so that we can add our own content above the table.
+   * parent::render() is where EntityListBuilder creates the table using our
+   * buildHeader() and buildRow() implementations.
+   */
+  public function render() {
+    $build['description'] = array(
+      '#markup' => $this->t('OAuth2_Server implements a Clients model. These clients are fieldable entities. You can manage the fields on the <a href="@adminlink">Clients admin page</a>.', array(
+        '@adminlink' => \Drupal::urlGenerator()->generateFromRoute('OAuth2_server.client_settings'),
+      )),
+    );
+    $build['table'] = parent::render();
+    return $build;
+  }
+
+  /**
+   * {@inheritdoc}
+   *
+   * Building the header and content lines for the client list.
+   *
+   * Calling the parent::buildHeader() adds a column for the possible actions
+   * and inserts the 'edit' and 'delete' links as defined for the entity type.
+   */
+  public function buildHeader() {
+    //@TODO - Change these to match the client entity
+    $header['id'] = $this->t('ContactID');
+    $header['name'] = $this->t('Name');
+    $header['first_name'] = $this->t('First Name');
+    $header['gender'] = $this->t('Gender');
+    return $header + parent::buildHeader();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildRow(EntityInterface $entity) {
+    //@TODO - Change these to match the client entity
+    /* @var $entity \Drupal\content_entity_example\Entity\Contact */
+    $row['id'] = $entity->id();
+    $row['name'] = $entity->link();
+    $row['first_name'] = $entity->first_name->value;
+    $row['gender'] = $entity->gender->value;
+    return $row + parent::buildRow($entity);
+  }
+}
---
+++ src/Form/ClientSettingsForm.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: eschisler
+ * Date: 27/07/15
+ * Time: 4:53 PM
+ */
+
+namespace Drupal\content_entity_example\Form;
+use
+
+  Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+class ClientSettingsForm extends FormBase {
+
+  /**
+   * Returns a unique string identifying the form.
+   *
+   * @return string
+   *   The unique string identifying the form.
+   */
+  public function getFormId() {
+    return 'oauth2_server_settings';
+  }
+
+  /**
+   * Form submission handler.
+   *
+   * @param array $form
+   *   An associative array containing the structure of the form.
+   * @param FormStateInterface $form_state
+   *   An associative array containing the current state of the form.
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    // Empty implementation of the abstract submit class.
+  }
+
+  /**
+   * Define the form used for OAuth2_Server settings.
+   * @return array
+   *   Form definition array.
+   *
+   * @param array $form
+   *   An associative array containing the structure of the form.
+   * @param FormStateInterface $form_state
+   *   An associative array containing the current state of the form.
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $form['contact_settings']['#markup'] = 'Settings form for OAuth2_Server. Manage field settings here.';
+    return $form;
+  }
+}
